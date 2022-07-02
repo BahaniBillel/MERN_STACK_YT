41cbc2936721fbdb9f0b45834b4602add0a9a2db
BahaniBillel <billel.bahani@gmail.com>
2022-07-02 23:28:04 +0100

    first commit

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+
+const express = require("express");
+const { default: mongoose } = require('mongoose');
+const workoutsRoutes=require("./routes/workouts")
+// express app
+
+const app = express()
+
+// middleware
+app.use(express.json())
+app.use((req,res,next)=>{
+    console.log(req.path,req.method)
+    next()
+})
+
+// routes
+app.use('/api/workouts',workoutsRoutes)
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+
+    app.listen(process.env.PORT,()=>{
+        console.log('connected to db, listening on port ',process.env.PORT)
+    })
+})
+.catch(()=>{
+    console.log(error);
+})
+
